bbf7b5db2c0f0fbbe2ea540597ca43a394bbfebd
oliveryh <oyerburyhodgson@gmail.com>
2020-02-02 15:11:33 +0000

    added response view_modifier

--- flask_tasks/app.py
+++ flask_tasks/app.py
@@ -1,11 +1,13 @@
 import flask
+from infrastructure.view_modifiers import response
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
+@response(template_file="home/index.html")
 def index():
-    return flask.render_template("home/index.html")
+    return {}
 
 
 if __name__ == "__main__":
