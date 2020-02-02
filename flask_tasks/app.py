5255511da4730a72a62467a2ea054a160ed1b188
oliveryh <oyerburyhodgson@gmail.com>
2020-02-02 15:04:44 +0000

    init jinja templating * added inherited index page * rendering template instead of Hello World placeholder

--- flask_tasks/app.py
+++ flask_tasks/app.py
@@ -2,9 +2,11 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return "Hello World!"
+    return flask.render_template("home/index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
