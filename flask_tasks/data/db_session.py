efd688c4ac21b48c9f24ee3551204ed0629f2477
oliveryh <oyerburyhodgson@gmail.com>
2020-02-03 22:45:34 +0000

    refactored db_session to include create_session function

--- flask_tasks/data/db_session.py
+++ flask_tasks/data/db_session.py
@@ -1,15 +1,16 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
 
 from flask_tasks.data.modelbase import SqlAlchemyBase
 
-factory = None
+__factory = None
 
 
 def global_init(db_file: str):
-    global factory
+    global __factory
 
-    if factory:
+    if __factory:
         return
 
     if not db_file or not db_file.strip():
@@ -18,8 +19,13 @@ def global_init(db_file: str):
     conn_str = "sqlite:///" + db_file.strip()
     engine = sa.create_engine(conn_str, echo=True)
 
-    factory = orm.sessionmaker(bind=engine)
+    __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
     import flask_tasks.data.__allmodels
     SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
